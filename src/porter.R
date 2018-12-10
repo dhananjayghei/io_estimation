66c936571d44ea5d09df234316ad51a789c1381d
dhananjay <djghei@gmail.com>
2018-12-09 19:09:42 -0600

    Adding the price war code.

--- src/porter.R
+++ src/porter.R
@@ -1,7 +1,10 @@
+# Sourcing the functions file
+source("functions.R")
 # Loading the required library
 library(AER) # For IV regression
 library(stargazer) # For LaTeX regression tables
 library(xtable) # For LaTeX tables
+library(lmtest) # For robust standard errors
 # Reading in the data set
 dat <- read.table("../data/PORTER.PRN")
 # Setting in the column names in order
@@ -18,7 +21,8 @@ sum_vars <- do.call(rbind, lapply(vars, function(x){
 sum_vars[2, ] <- round(sum_vars[2, ], 0)
 sum_vars <- data.frame(sum_vars)
 rownames(sum_vars) <- c("Price", "Quantity", "Lakes", "Collusion")
-
+genxtable(xtable(sum_vars, align="lrrrr", digits=c(0, rep(3,4))),
+          basename="sumstats_porter", include.rownames=TRUE)
 # Generating log variables
 dat$logQ <- log(dat$quantity)
 dat$logP <- log(dat$price)
@@ -37,19 +41,56 @@ iv_collusion <- ivreg(logQ~lakes+seasonal+logP | lakes+seasonal+collusion, data=
 iv_collusion_dv <- ivreg(logQ~lakes+seasonal+logP | lakes+seasonal+collusion+d1+d2+d3+d4, data=dat)
 
 # Estimating the supply equation
-ols_supply <- lm(logP~d1+d2+d3+d4+logQ+collusion, data=dat)
+ols_supply <- lm(logP~d1+d2+d3+d4+collusion+logQ, data=dat)
 # Run IV with lakes as instrument for log quantity
 iv_supply <- ivreg(logP~d1+d2+d3+d4+seasonal+collusion+logQ | d1+d2+d3+d4+seasonal+collusion+lakes, data=dat)
+
+stargazer(ols, type="latex", float=FALSE, keep=1:3)
 
 # Storing the regression results (Demand equation)
 sink(file="../doc/tables/porter_demand.gen")
 stargazer(ols, iv_collusion, iv_collusion_dv,
-          type="latex", keep=c("logP", "lakes"),
-          covariate.labels=c("Log(Price)", "Lakes"),
-          floating=FALSE)
+          type="latex", covariate.labels=c("Constant", "Log(Price)", "Lakes", paste0("SEAS", 1:12)),
+          column.labels=c("OLS", "IV", "IV"), intercept.bottom=FALSE, dep.var.labels="",
+          model.names=FALSE,
+          float=FALSE)
 sink()
 
-## # Markov-switching regime
-## library(MSwM)
+# Storing the regrssion results (Supply equation)
+sink(file="../doc/tables/porter_supply.gen")
+stargazer(ols_supply, iv_supply,
+          type="latex", covariate.labels=c("Constant", paste0("D", 1:4), paste0("SEAS", 1:12),
+                                           "Collusion", "Log(Quantity)"),
+          column.labels=c("OLS", "IV"), intercept.bottom=FALSE, dep.var.labels="",
+          model.names=FALSE,
+          float=FALSE)
+sink()
 
-## kk <- msmFit(object=ols_supply, k=2, sw=c(rep(TRUE, 7), TRUE), control=list(parallel=FALSE))
+# Figure 1 (Porter)
+pdf(file="../doc/pics/fig1_porter.pdf", width=5.4, height=3.8)
+par(mai=c(.8,.8,.3,.2))
+plot(dat$week, dat$price, type="l", lwd=2, main="", xlab="Time (in weeks) from January 1, 1980",
+     ylab="Grain rate", las=1)
+dev.off()
+
+# Cause of price-wars
+# Generate an indicator variable for price war
+
+dat$price_war <- as.numeric(c(FALSE, sapply(2:length(dat$collusion), function(i)
+    dat$collusion[i] < dat$collusion[(i-1)])))
+
+# Run probit model
+options(scipen=12)
+probit <- glm(price_war~quantity+lakes+d1+d2+d3+d4, family=binomial(link="probit"), data=dat)
+# Getting the robust standard errors
+probit.se <- vcovHC(probit, type="HC1")
+probit.se <- sqrt(diag(probit.se))
+
+sink(file="../doc/tables/probit_porter.gen")
+stargazer(probit, type="latex", se=list(probit.se),
+          dep.var.labels="Price war",
+          covariate.labels=c("Constant", "Quantity", "Lakes", paste0("D", 1:4)),
+          intercept.bottom=FALSE, float=FALSE)
+sink()
+
+
